6c38df84470175e789306643023357f11f9e6dd7
Toan-Nguyen26 <toan.nguyenhai2607@gmail.com>
2024-08-07 20:54:32 +0700

    Clean up

--- todos/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('details/<int:id>', views.details, name='details'),
-    path('add/', views.add, name='add')
-]
