abc9a350fd7fd7704016ee37c105eb439471dbaf
Toan-Nguyen26 <toan.nguyenhai2607@gmail.com>
2024-08-07 21:28:40 +0700

    More updates

--- diabetes/urls.py
+++ diabetes/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('predict', views.predict, name='predict'),
+    path("", views.index, name="index"),
+    # path('predict', views.predict, name='predict'),
 ]
